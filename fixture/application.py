9e74489d11ed5323b48b64e3bce1cb0b262709ee
Денис <shanterrr@yandex.ru>
2018-11-14 17:41:58 +0300

    Выделен класс для работы с контактами

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
+
 
 class Application():
 
@@ -10,97 +11,10 @@ class Application():
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def click_add_new(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
+        self.contact = ContactHelper(self)
 
     def destroy(self):
         self.wd.quit()
-
-    def try_to_upload_foto(self):
-        wd = self.wd
-        wd.find_element_by_name("photo").send_keys(r"C:\PycharmProjects\123\OklqbFQ_dbA.jpg")
-
-    def create_new_contact(self, contact):
-        wd = self.wd
-        self.click_add_new()
-        self.try_to_upload_foto()
-        # Заполняет данные в форму
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work)
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        wd.find_element_by_name("submit").click()
 
     def open_homepage(self):
         wd = self.wd
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="Denis", middlename="Butylin", lastname="lastname",
-                                    nickname="nickname", title="title", company="company", address="address",
-                                    home="home", mobile="mobile", work="work", fax="fax",
-                                    email="email1", email2="email2", email3="email3",
-                                    homepage="homepage",  bday="29", bmonth="April", byear="1992",
-                                    aday="1", amonth="January", ayear="2000",
-                                    address2="address2", phone2="phone2", notes="notes"))
+    app.contact.create_new(Contact(firstname="Denis", middlename="Butylin", lastname="lastname",
+                                   nickname="nickname", title="title", company="company", address="address",
+                                   home="home", mobile="mobile", work="work", fax="fax",
+                                   email="email1", email2="email2", email3="email3",
+                                   homepage="homepage", bday="29", bmonth="April", byear="1992",
+                                   aday="1", amonth="January", ayear="2000",
+                                   address2="address2", phone2="phone2", notes="notes"))
     app.session.logout()
 
