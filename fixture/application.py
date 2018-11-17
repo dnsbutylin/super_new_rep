5d4530e19d5fbd19a36f6122d480329ebfc19210
Денис <shanterrr@yandex.ru>
2018-11-18 02:19:24 +0300

    Повышена интеллектуальность фикстуры и методов логин и логаут

--- fixture/application.py
+++ fixture/application.py
@@ -16,6 +16,13 @@ class Application():
     def destroy(self):
         self.wd.quit()
 
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
     def open_homepage(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,8 +2,7 @@
 from model.contact import Contact
 
 
-def test_add_new_contact(app):
-    app.session.login(username="admin", password="secret")
+def test_add_contact(app):
     app.contact.create(Contact(firstname="Denis", middlename="Butylin", lastname="lastname",
                                nickname="nickname", photo=r"C:\PycharmProjects\123\OklqbFQ_dbA.jpg", title="title", company="company", address="address",
                                home="home", mobile="mobile", work="work", fax="fax",
@@ -11,5 +10,3 @@ def test_add_new_contact(app):
                                homepage="homepage", bday="29", bmonth="April", byear="1992",
                                aday="1", amonth="January", ayear="2000",
                                address2="address2", phone2="phone2", notes="notes"))
-    app.session.logout()
-
