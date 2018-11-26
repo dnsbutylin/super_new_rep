40cc5957e40e7d719b02188b6b9a0ada53eaad9b
dnsbutylin <44590596+dnsbutylin@users.noreply.github.com>
2018-11-26 17:58:04 +0300

    Добавлены проверки для тестов с группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,9 +3,15 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="sadsdasda", header="sadassdadasd", footer="sfaffafa"))
+    new_group = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_group)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_group = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_group)
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,9 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
     app.group.modify_first_group(Group(name="New name"))
-
+    new_group = app.group.get_group_list()
+    assert len(old_groups) == len(new_group)
