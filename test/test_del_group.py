5ce9da0729e71cf980424aecbce4829d3af04c8e
dnsbutylin <44590596+dnsbutylin@users.noreply.github.com>
2018-11-26 21:39:51 +0300

    Добавлены проверки для тестов с группами

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -6,5 +6,11 @@ def test_delete_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name='test'))
     app.group.delete_first_group()
-    new_group = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_group)
+    new_groups = app.group.get_group_list()
+    if len(old_groups) > 0:
+        assert len(old_groups) - 1 == len(new_groups)
+    else:
+        assert len(old_groups) == len(new_groups)
+    old_groups[0:1] = []
+    assert old_groups == new_groups
+
