badd068e48680520e6a051fe784e1743bb2e9ec5
Денис <shanterrr@yandex.ru>
2018-12-02 18:45:30 +0300

    Добавлено кеширование и хеширование при загрузке списка групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -17,6 +17,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         # Нажимаем подтвердить
         wd.find_element_by_name("submit").click()
+        self.contact_cache = None
 
     def fill_contact_form(self, contact):
         # Заполняет данные в форму
@@ -63,6 +64,7 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
         self.open_contact_list()
+        self.contact_cache = None
 
     def modify_first_contact(self, new_contact_data):
         wd = self.app.wd
@@ -74,6 +76,7 @@ class ContactHelper:
         self.fill_contact_form(new_contact_data)
         # Нажимаем кнопку update
         wd.find_element_by_name("update").click()
+        self.contact_cache = None
 
     def select_first_contact(self):
         wd = self.app.wd
@@ -90,16 +93,20 @@ class ContactHelper:
         self.open_contact_list()
         return len(wd.find_elements_by_name('selected[]'))
 
+    contact_cache = None
+
     def get_contact_list(self):
-        wd = self.app.wd
-        self.open_contact_list()
-        contacts = []
-        for element in wd.find_elements_by_name('entry'):
-            cells = element.find_elements_by_tag_name("td")
-            id = cells[0].find_element_by_name('selected[]').get_attribute('value')
-            lastname = cells[1].text
-            firstname = cells[2].text
-            contacts.append(Contact(firstname=firstname, lastname=lastname, id=id))
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.open_contact_list()
+            self.contact_cache = []
+            for element in wd.find_elements_by_name('entry'):
+                cells = element.find_elements_by_tag_name("td")
+                id = cells[0].find_element_by_name('selected[]').get_attribute('value')
+                lastname = cells[1].text
+                firstname = cells[2].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+        # Возвращаем копию кеша
+        return list(self.contact_cache)
 
 
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,8 +14,8 @@ def test_add_contact(app):
                                aday="1", amonth="January", ayear="2000",
                                address2="address2", phone2="phone2", notes="notes")
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key = Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
