1ed0854cecd6185037faa944adf0dfb4081f4e4c
Денис <shanterrr@yandex.ru>
2018-12-07 00:17:45 +0300

    Добавлен тесты проверки телефонов

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -78,9 +79,7 @@ class ContactHelper:
         self.select_contact_by_index(index)
         # Нажимаем нужный карандашик по индексу выбранного элемента + 1 (редактировать)
         wd.find_element_by_xpath("(//img[@alt='Edit'])[" + str(index+1) + "]").click()
-        #Заполняем новые данные в форму
         self.fill_contact_form(new_contact_data)
-        # Нажимаем кнопку update
         wd.find_element_by_name("update").click()
         self.contact_cache = None
 
@@ -115,8 +114,65 @@ class ContactHelper:
                 id = cells[0].find_element_by_name('selected[]').get_attribute('value')
                 lastname = cells[1].text
                 firstname = cells[2].text
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+                all_phones = cells[5].text
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  all_phones_from_home_page=all_phones))
         # Возвращаем копию кеша
         return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_homepage()
+        row = wd.find_elements_by_name('entry')[index]
+        cell = wd.find_elements_by_tag_name('td')[7]
+        cell.find_element_by_tag_name('a').click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_homepage()
+        row = wd.find_elements_by_name('entry')[index]
+        cell = wd.find_elements_by_tag_name('td')[6]
+        cell.find_element_by_tag_name('a').click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name('firstname').get_attribute('value')
+        lastname = wd.find_element_by_name('lastname').get_attribute('value')
+        id = wd.find_element_by_name('id').get_attribute('value')
+        home = wd.find_element_by_name('home').get_attribute('value')
+        work = wd.find_element_by_name('work').get_attribute('value')
+        mobile = wd.find_element_by_name('mobile').get_attribute('value')
+        phone2 = wd.find_element_by_name('phone2').get_attribute('value')
+        return Contact(firstname=firstname, lastname=lastname, id=id,
+                       home=home, work=work,
+                       mobile=mobile, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        def clear(s):
+            return re.sub("[() -]", "", s)
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id('content').text
+        try:
+            home = re.search('H: (.*)', text).group(1)
+        except AttributeError:
+            home = ''
+        try:
+            work = re.search('W: (.*)', text).group(1)
+        except AttributeError:
+            work = ''
+        try:
+            mobile = re.search('M: (.*)', text).group(1)
+        except AttributeError:
+            mobile = ''
+        try:
+            phone2 = re.search('P: (.*)', text).group(1)
+        except AttributeError:
+            phone2 = ''
+        return Contact(all_phones_from_home_page='\n'.join(filter(lambda x: x !='',
+                                map(lambda x: clear(x),
+                                    filter(lambda x: x is not None,
+                                           [home, mobile, work, phone2])))))
 
 
--- model/contact.py
+++ model/contact.py
@@ -10,7 +10,7 @@ class Contact:
                  email3=None, homepage=None, bday=None,
                  bmonth=None, byear=None, aday=None,
                  amonth=None, ayear=None, address2=None,
-                 phone2=None, notes=None, id=None):
+                 phone2=None, notes=None, id=None, all_phones_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -37,6 +37,7 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return '%s:%s:%s' % (self.id, self.firstname, self.lastname)
@@ -44,7 +45,6 @@ class Contact:
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) \
                and self.firstname == other.firstname and self.lastname == other.lastname
-
 
     def id_or_max(self):
         if self.id:
