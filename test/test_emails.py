55d50b4c88984130e7c6e103f898e53f621b97a4
Денис <shanterrr@yandex.ru>
2018-12-07 03:48:12 +0300

    Добавлены тесты проверки контакта

--- test/test_emails.py
+++ test/test_emails.py
@@ -1,4 +1,3 @@
-import re
 
 
 def test_emails_on_home_page(app):
@@ -6,10 +5,11 @@ def test_emails_on_home_page(app):
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
-def test_emails_on_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+# Рабочий, но лишний
+#def test_emails_on_view_page(app):
+#    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+#    assert contact_from_view_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 def merge_emails_like_on_home_page(contact):
     return '\n'.join(filter(lambda x: x !='',
