e639d437d57ed4c0b10d8fe63dfa09c67294441b
dnsbutylin <44590596+dnsbutylin@users.noreply.github.com>
2018-12-18 20:56:02 +0300

    последний комит для отправки 3:34 о конца последнего видео

--- check_db_connection.py
+++ check_db_connection.py
@@ -12,12 +12,24 @@
 #    db.destroy()
 
 from fixture.orm import ORMFixture
+from model.group import Group
+
+#db = ORMFixture(host='127.0.0.1', name='addressbook', user='root',
+#                             password='')
+
+#try:
+#    l = db.get_contact_list()
+#    for item in l:
+#        print(item)
+#    print(len(l))
+#finally:
+#    pass #db.destroy()
 
 db = ORMFixture(host='127.0.0.1', name='addressbook', user='root',
                              password='')
 
 try:
-    l = db.get_group_list()
+    l = db.get_contacts_in_group(Group(id='11'))
     for item in l:
         print(item)
     print(len(l))
