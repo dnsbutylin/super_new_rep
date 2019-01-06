4de3cfa0cfe30bbd2b1e6f7fea4100cde5b2613c
Admin <shanterrr@yandex.ru>
2019-01-06 23:46:44 +0300

    последний комит

--- fixture/session.py
+++ fixture/session.py
@@ -37,6 +37,7 @@ class SessionHelper:
     def get_logged_user(self):
         wd = self.app.wd
         return wd.find_element_by_xpath("//div[@id='top']/form/b").text[1:-1]
+
     def ensure_login(self, username, password):
         wd = self.app.wd
         #self.app.open_homepage()
