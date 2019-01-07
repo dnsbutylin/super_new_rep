3290683acd937bd2b8782d1c8d2840369c33845e
shant <shanterrr@yandex.ru>
2019-01-07 15:55:20 +0300

    доработаны тесты доавления контакта в группу и удаления из группы.

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -1,15 +1,30 @@
 from model.contact import Contact
-
+from model.group import Group
 import random
+
 
 def test_add_contact_to_group(app, orm):
     if len(orm.get_contact_list()) == 0:
         app.contact.create(Contact(firstname='test_firstname', lastname='test_lastname',
                                    address='test_address'))
-    # Оставляю выбор группы и контакта случайным, так как если контакт в группе уже есть,
-    # ничего не происходит (т.е. повторно он не добавляется), и тест завршится при этом положительно
+    if len(orm.get_group_list()) == 0:
+        app.group.create(Group(name='test_name1', header='test_header1', footer='test_footer1'))
     contact = random.choice(orm.get_contact_list())
-    group = random.choice(orm.get_group_list())
+    # Пошел цикл по всем группам, если контакт есть во всех группах, то ничего, иначе добавит контакт в группу
+    i = 0
+    group = orm.get_group_list()[i]
+    while i < len(orm.get_group_list()):
+        group = orm.get_group_list()[i]
+        if contact in orm.get_contacts_not_in_group(group):
+            app.contact.add_to_group(contact.id, group.id)
+            break
+        i += 1
+    # Если выбранный контакт есть во всех группах, добавляем новый контакт, добавляем его в группу
+    if i == len(orm.get_group_list()):
+        app.contact.create(Contact(firstname='test_firstname1', lastname='test_lastname1',
+                                   address='test_address1'))
+    contact = sorted(orm.get_contact_list(), key=lambda contact: int(contact.id) ,reverse=True)[0]
     app.contact.add_to_group(contact.id, group.id)
+
     l = orm.get_contacts_in_group(group)
     assert contact in l
--- test/test_delete_contact_from_group.py
+++ test/test_delete_contact_from_group.py
@@ -1,12 +1,22 @@
 import random
+from model.contact import Contact
+from model.group import Group
 
 
 def test_delete_contact_from_group(app, orm):
+    if len(orm.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname='test_firstname4', lastname='test_lastname4',
+                                   address='test_address4'))
     group = random.choice(orm.get_group_list())
     l = orm.get_contacts_in_group(group)
+    # Если контактов нет в группе, создаем новый, добавляем его в группу, затем удаляем
     if l == []:
-        # Если список контактов в группе пуст, пусть проверка будет положительная
-        assert True
+        app.contact.create(Contact(firstname='test_firstname2', lastname='test_lastname2',
+                                   address='test_address2'))
+        contact = sorted(orm.get_contact_list(), key=lambda contact: int(contact.id), reverse=True)[0]
+        app.contact.add_to_group(contact.id, group.id)
+        assert contact in orm.get_contacts_not_in_group(group)
+    # Если контакты есть в группе, выбираем случайный, затем удалям
     else:
         contact = random.choice(l)
         app.contact.delete_contact_from_group(contact.id, group.id)
